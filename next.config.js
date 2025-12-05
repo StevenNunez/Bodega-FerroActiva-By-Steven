8459c697632fdd9d963f973904ed1bb067658231
StevenNunez <anthonystevenjulian@gmail.com>
2025-12-05 16:12:05 -0300

    Security Check Recommended (CVE-2025-55182_ok)

--- next.config.js
+++ next.config.js
@@ -1,31 +1,6 @@
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+// next.config.js
+export default {
   experimental: {
-    serverComponentsExternalPackages: ['exceljs'],
+    turbopack: false, // ESTO ES TODO LO QUE NECESITAS
   },
 };
-
-module.exports = nextConfig;
